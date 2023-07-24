91ebd7fd223ef0241ee3d8e1cba747e15a2513b7
Elvis Ibarra <ielvisd@gmail.com>
2023-07-24 15:05:24 -0700

    Unlock message works + router to txid unlock page

--- powcano-scrypt/src/views/UnlockView.vue
+++ powcano-scrypt/src/views/UnlockView.vue
@@ -1,35 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="about">
+    <h1>This is the unlock page</h1>
+    <!-- The TXID of the contract you are trying to unlock -->
+    <!-- From the path that we are on '/:id' -->
+    <p>TXID: {{ route.params.id }}</p>
+
     <!-- Message input -->
-    <input type="text" v-model="message" />
+    <input type="text" v-model="unlockMessage" />
     <!-- Login button -->
     <button @click="login">Login</button>
     <!-- Deploy Contract -->
-    <button @click="deploy">Deploy</button>
+    <button @click="unlock">Unlock</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+const unlockMessage = ref(null)
+const contractTXID = ref(route.params.id)
+
 import { Helloworld } from '../contracts/helloworld';
 import artifact from '../../artifacts/helloworld.json';
 import { sha256, toByteString, DefaultProvider, SensiletSigner, } from 'scrypt-ts';
 
 const signer = ref<SensiletSigner>();
 const instance = ref<Helloworld>();
-const message = ref<string>('');
 
 onMounted(() => {
-  console.log('Home view mounted');
-
+  console.log('Unlock txid view mounted');
   Helloworld.loadArtifact(artifact)
 
   console.log('Helloworld', Helloworld);
-
   const provider = new DefaultProvider({});
   signer.value = new SensiletSigner(provider);
-});
+})
 
 
 
@@ -51,6 +60,20 @@ const login = async () => {
       console.log('User address', userAddress);
       alert('Logged in user is: ' + userAddress)
       console.log('Connected to Scrypt smart contract instance', instance.value);
+
+      if (contractTXID.value) {
+        console.log('contractTXID', contractTXID.value[0])
+        const tx = await signer.value.connectedProvider.getTransaction(contractTXID.value)
+        instance.value = Helloworld.fromTx(tx, 0)
+
+        console.log('instance', instance);
+
+        const provider = new DefaultProvider({});
+        signer.value = new SensiletSigner(provider);
+
+        await instance.value.connect(signer.value);
+      }
+
       // ...
     } catch (error) {
       alert('Sensilet error is: ' + error)
@@ -60,24 +83,14 @@ const login = async () => {
 }
 
 // deploy function
-const deploy = async() => {
-  console.log('Deploy function', instance.value, signer.value)
-
-  // create a new instance of the contract
-  const messageInBytes = toByteString(message.value, true)
-  instance.value = new Helloworld(sha256(messageInBytes))
-
-  // connect the instance to the signer & deploy the contract
-  if(instance.value && signer.value) {
-    await instance.value.connect(signer.value);
-
-    console.log('Scrypt smart contract instance', instance.value);
-
-    // deploy the contract and lock up 42 satoshis in it
-    const deployTx = await instance.value.deploy(42)
-    console.log('Helloworld contract deployed: ', deployTx.id)
-    alert('Helloworld contract deployed: ' + deployTx.id)
+const unlock = async() => {
+  console.log('Unlock function', instance.value, signer.value, unlockMessage.value)
+  if(instance.value && unlockMessage.value) {
+  // Call the 'unlock' method of our deployed HelloWorld contract instance
+  const hexUnlockMessage = toByteString(unlockMessage.value, true)
+  const { tx: callTx } = await instance.value.methods.unlock(hexUnlockMessage)
+  console.log('Helloworld contract `unlock` called: ', callTx.id)
   }
-
 }
 </script>
+
